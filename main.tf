88d4e8eb689bd99725370f512c010259a617021f
Hoang Phung <60034216+hphun9@users.noreply.github.com>
2025-04-30 19:56:28 +0700

    [hphun9] Add variable for avoid hardcore values

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 module "vpc" {
   source      = "./modules/vpc"
   vpc_cidr    = "10.0.0.0/16"
-  name_prefix = "cnmeow"
+  name_prefix = var.prefix_tag_name
 }
 
 module "subnet" {
   source              = "./modules/subnet"
   vpc_id              = module.vpc.vpc_id
-  name_prefix         = "cnmeow"
+  name_prefix         = var.prefix_tag_name
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
   availability_zone   = var.aws_region
@@ -16,13 +16,13 @@ module "subnet" {
 module "internet_gateway" {
   source      = "./modules/internet_gateway"
   vpc_id      = module.vpc.vpc_id
-  name_prefix = "cnmeow"
+  name_prefix = var.prefix_tag_name
 }
 
 module "nat_gateway" {
   source            = "./modules/nat_gateway"
   public_subnet_id  = module.subnet.public_subnet_id
-  name_prefix       = "cnmeow"
+  name_prefix       = var.prefix_tag_name
 }
 
 module "route_table" {
@@ -32,23 +32,23 @@ module "route_table" {
   nat_gateway_id      = module.nat_gateway.nat_gateway_id
   public_subnet_id    = module.subnet.public_subnet_id
   private_subnet_id   = module.subnet.private_subnet_id
-  name_prefix         = "cnmeow"
+  name_prefix         = var.prefix_tag_name
 }
 
 module "security_group" {
   source = "./modules/security_group"
   vpc_id      = module.vpc.vpc_id
-  allowed_ip  = "42.118.53.80/32"
-  name_prefix = "cnmeow"
+  allowed_ip  = var.allowed_ip
+  name_prefix = var.prefix_tag_name
 }
 
 module "ec2" {
   source             = "./modules/ec2"
-  ami_id             = "<AMI_Image>"
-  instance_type      = "t3.micro"
+  ami_id             = var.ec2_ami
+  instance_type      = var.ec2_instance_type
   public_subnet_id   = module.subnet.public_subnet_id
   private_subnet_id  = module.subnet.private_subnet_id
   public_sg_id       = module.security_group.public_sg_id
   private_sg_id      = module.security_group.private_sg_id
-  name_prefix        = "cnmeow"
+  name_prefix        = var.prefix_tag_name
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,23 @@ variable "aws_region" {
   description = "AWS Region"
   default     = "ap-southeast-1"
 }
+
+variable "ec2_ami" {
+  description = "EC2 AMI"
+  default = ""
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance Type"
+  default = "t3.micro"
+}
+
+variable "prefix_tag_name" {
+  description = "Prefix tag name for resources"
+  default = "cnmeow"
+}
+
+variable "allowed_ip" {
+  description = "Your trusted IP for SSH access in CIDR format (example: 1.2.3.4/32)."
+  type        = string
+}
